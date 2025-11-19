023da6689aa51c95ebfc182d5b257fb21da94b90
Davide D'Ambrosio <davide431@outlook.it>
2025-11-20 00:23:34 +0100

    fix: revert to standard TinaCMSProvider with type casting to resolve initialization issues

--- src/app/(admin)/admin/TinaWrapper.tsx
+++ src/app/(admin)/admin/TinaWrapper.tsx
@@ -1,25 +1,20 @@
 'use client'
 
-import { TinaCMS, TinaProvider, TinaAdmin } from 'tinacms'
+import TinaCMSProvider from 'tinacms'
+import { TinaAdmin } from 'tinacms'
 import config from '../../../../tina/config'
 import tinaClient from '../../../../tina/__generated__/client'
-import { useMemo } from 'react'
 
 export default function TinaWrapper() {
   const tinaGraphqlVersion = process.env.NEXT_PUBLIC_TINA_GRAPHQL_VERSION || '1.4'
 
-  const cms = useMemo(() => {
-    const cmsInstance = new TinaCMS({
-      enabled: true,
-      sidebar: true,
-    })
-    cmsInstance.registerApi('tina', tinaClient)
-    return cmsInstance
-  }, [])
-
   return (
-    <TinaProvider cms={cms}>
+    <TinaCMSProvider
+      client={tinaClient as any}
+      tinaGraphQLVersion={tinaGraphqlVersion}
+      {...(config as any)}
+    >
       <TinaAdmin config={config} />
-    </TinaProvider>
+    </TinaCMSProvider>
   )
 }
